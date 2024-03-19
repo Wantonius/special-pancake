0a382502f14e0efa118b8812d5ef91729e365317
Erno Hentonen <erno.hentonen@opiframe.com>
2024-03-19 17:53:19 +0200

    cardback

--- 01_memory_game/src/components/MemoryCard.jsx
+++ 01_memory_game/src/components/MemoryCard.jsx
@@ -1,17 +1,51 @@
 import cardback from '../assets/cardback.png'
+import {useState,useEffect} from 'react'
 
 const MemoryCard = (props) => {
+
+    const [state,setState] = useState({
+        isOpen:false
+    })
+
+    const turnCard = (event) => {
+        if(!state.isOpen) {
+            setState({
+                isOpen:true
+            })
+        }
+    } 
+
+    useEffect(() => {
+        if(state.isOpen) {
+            setTimeout(turnBack,2000)
+        }
+    },[state.isOpen])
+
+    const turnBack = () => {
+        setState({
+            isOpen:false
+        })
+    }
 
     const cardStyle = {
         height:200,
         width:144,
-        margin:5
+        margin:5,
+        backgroundColor:"red"
     }
-    return (
-        <div style={cardStyle}>
-            <img src={cardback} height="200" width="144" />
-        </div>
-    )
+
+    if(state.isOpen) {
+        return (
+            <div style={cardStyle}>
+            </div>
+        )
+    } else {
+        return (
+            <div style={cardStyle} onClick={turnCard}>
+                <img src={cardback} height="200" width="144" />
+            </div>
+        )
+    }
 }
 
 export default MemoryCard
