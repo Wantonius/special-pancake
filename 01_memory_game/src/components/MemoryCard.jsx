68cb2267187c406460f56559e445207651f771a0
Erno Hentonen <erno.hentonen@opiframe.com>
2024-03-19 17:57:52 +0200

    cardback

--- 01_memory_game/src/components/MemoryCard.jsx
+++ 01_memory_game/src/components/MemoryCard.jsx
@@ -3,9 +3,13 @@ import {useState,useEffect} from 'react'
 
 const MemoryCard = (props) => {
 
+    //Komponentin tila, joka määrää kummin päin kortti on 
+
     const [state,setState] = useState({
         isOpen:false
     })
+
+    //Funktio, joka on kiinnitetty korttien (div) click eventtiin. Kääntää kortin oikein päin (punainen puoli ylös)
 
     const turnCard = (event) => {
         if(!state.isOpen) {
@@ -15,11 +19,16 @@ const MemoryCard = (props) => {
         }
     } 
 
+    //UseEffect joka laukeaa, kun kortin tilaa muutetaan. Starttaa kahden sekunnin timeoutin, jos kortti on oikeinpäin. Timeout 
+    //kutsuu funktiota turnBack, joka kääntää kortin takaisin väärinpäin (kuvapuoli ylös)
+
     useEffect(() => {
         if(state.isOpen) {
             setTimeout(turnBack,2000)
         }
     },[state.isOpen])
+
+    //Kääntää kortin takaisin kuvapuoli ylös eli väärinpäin
 
     const turnBack = () => {
         setState({
@@ -33,6 +42,9 @@ const MemoryCard = (props) => {
         margin:5,
         backgroundColor:"red"
     }
+
+    //Konditionaalinen renderöinti, jossa riippuen kortin tilasta kortti joko piirretään punainen puoli tai
+    //kuvapuoli ylöspäin
 
     if(state.isOpen) {
         return (
