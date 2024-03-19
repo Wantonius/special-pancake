fd1ba1f86e922da75d40e99a793495034c9a6043
Erno Hentonen <erno.hentonen@opiframe.com>
2024-03-19 17:08:31 +0200

    cardback

---
+++ 01_memory_game/src/components/MemoryCard.jsx
@@ -0,0 +1,15 @@
+const MemoryCard = (props) => {
+
+    const cardStyle = {
+        height:200,
+        width:144,
+        margin:5
+    }
+    return (
+        <div style={cardStyle}>
+            <img src="../assets/cardback.png" />
+        </div>
+    )
+}
+
+export default MemoryCard
