06f9d99ab39f8de1a37d69c11129fc0c7b639bf4
Erno Hentonen <erno.hentonen@opiframe.com>
2024-03-19 17:30:21 +0200

    cardback

--- 01_memory_game/src/App.jsx
+++ 01_memory_game/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import MemoryCard from './components/MemoryCard'
+import "./App.css"
 
 function App() {
  
+  const cards = []
+  for (let i=0;i<12;i++) {
+    cards.push(<MemoryCard></MemoryCard>)
+  }
+
 
   return (
-    <>
-      <MemoryCard/>
-    </>
+    <div className="container">
+      {cards}
+    </div>
   )
 }
 
